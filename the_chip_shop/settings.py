fb9894340a626ffc53497182cc46eefc1be9fe65
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-03-10 18:08:45 +0000

    build: Add Allowed host

--- the_chip_shop/settings.py
+++ the_chip_shop/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-chip-shop-pp5-7f84f31ffc3b.herokuapp.com',
+                'localhost','127.0.0.1']
 
 
 # Application definition
