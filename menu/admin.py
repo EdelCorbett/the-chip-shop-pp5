2afd09ad461fc9a1db61da52d74b8591247aa52c
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-03-11 02:41:03 +0000

    build: Customise admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Category, menuitem
 
+
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(menuitem)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+class menuitemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(menuitem, menuitemAdmin)
