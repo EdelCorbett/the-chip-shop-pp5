ef624d3510464533643bd34166c6fcda25427788
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-03-11 03:01:51 +0000

    build: Add menu view and templates

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import menuitem
 
 # Create your views here.
+def all_menu(request):
+    """ A view to return the index page """
+
+    menu = menuitem.objects.all()
+
+    context = {
+        'menu': menu,
+    }
+
+    return render(request, 'menu/menu.html', context)
