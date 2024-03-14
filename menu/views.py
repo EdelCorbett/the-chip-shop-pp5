149600c48d1cdbe5748381a11995d5c4a5a056bc
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-03-14 13:54:07 +0000

    build: Add link to home page

--- menu/views.py
+++ menu/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from .models import menuitem
+from .models import menuitem, Category
 
 # Create your views here.
 def all_menu(request):
     """ A view to return the index page """
 
     menu = menuitem.objects.all()
+    categories = Category.objects.all() 
+    
 
     context = {
         'menu': menu,
+        'categories': categories,
+
+        
     }
 
     return render(request, 'menu/menu.html', context)
