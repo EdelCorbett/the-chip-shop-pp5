907e47b462fe82b300a3f3164d39f6aaaf58a06d
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-03-17 13:51:09 +0000

    build: Add Sort by button and functionality

--- menu/views.py
+++ menu/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.conf import settings
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Category, Menuitem
 
 # Create your views here.
@@ -10,10 +11,27 @@ def all_menu(request):
     """ A view to return menu page"""
 
     menu = Menuitem.objects.all()
-    category = None
+    category = Category.objects.all()
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                menu = menu.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            menu = menu.order_by(sortkey)
+
 
         if 'category' in request.GET:
             category = request.GET['category'].split(',')
@@ -30,13 +48,14 @@ def all_menu(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             menu = menu.filter(queries)
 
-    
-    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'menu': menu,
         'search_term': query,
         'categories': category,
+        'current_categories': category,
+        'current_sorting': current_sorting,
         'MEDIA_URL': settings.MEDIA_URL,
 
         
