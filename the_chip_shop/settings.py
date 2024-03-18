c9c0658b82c8ccf63d0dd6bd2ce68045878c4d4d
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-03-18 14:55:29 +0000

    build: Add toasts

--- the_chip_shop/settings.py
+++ the_chip_shop/settings.py
@@ -111,6 +111,8 @@ else:
         }
     }
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
