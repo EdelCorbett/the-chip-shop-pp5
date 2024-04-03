db576eb61ee91172deb43b72115cf17f4e8d1705
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-04-03 17:32:18 +0100

    build:Add form validation form iquiry form

--- inquiry/forms.py
+++ inquiry/forms.py
@@ -11,3 +11,25 @@ class InquiryForm(forms.ModelForm):
             'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}),
             'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message'}),
         }
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if not phone.isdigit():
+            raise forms.ValidationError('Phone number must be all digits.')
+            
+        if len(phone) < 10:
+            raise forms.ValidationError('Phone number must be at least 10 digits.')
+        return phone
+    
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if not message:
+            raise forms.ValidationError('Message cannot be empty.')
+
+        if len(message) < 10:
+            raise forms.ValidationError('Message must be at least 10 characters long.')
+
+        if len(message) > 500:
+            raise forms.ValidationError('Message cannot be more than 500 characters long.')
+
+        return message
